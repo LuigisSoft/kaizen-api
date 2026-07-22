33023ab2c90f2994c94c930dca57c195bba5f74b
Luis <tarrlin@gmail.com>
2026-07-22 18:01:21 +0200

    Initial commit: Spring Boot project created

---
+++ src/test/java/com/luigisback/kaizen_api/KaizenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigisback.kaizen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
